54ed765f972897a1cd0f29d9b94e387396758a18
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-01-15 20:06:17 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fixa import Test, Agent, Scenario
+from fixa.agents import Jessica
+
+jessica = Agent(
+    prompt="you are a young woman named lily who says 'like' a lot",
+    model="gpt-4o",
+    voice="jessica",
+)
+
+order_donut = Scenario(
+    prompt="order a dozen donuts with sprinkles and a coffee",
+    evaluations=[
+        "the order was successful",
+        "the agent confirmed the price of the order",
+    ],
+)
+
+tests = []
+test = Test(order_donut, jessica)
+test.run()
