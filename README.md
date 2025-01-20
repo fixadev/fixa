1d79d17c82e3d81da81f545c5ad143ca429ada1e
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-01-19 21:29:00 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,92 @@
 # fixa
 
-A tool for testing and evaluating AI voice agents
+a tool for testing and evaluating AI voice agents.
+
+use a voice agent to call your voice agent with an llm as a judge.
+
+# quick start
+
+with pip:
+
+```bash
+pip install fixa-dev
+```
+
+run a test:
+
+```python
+from fixa import Test, Agent, Scenario, Evaluation, TestRunner
+from fixa.evaluators import LocalEvaluator
+from dotenv import load_dotenv
+import ngrok, os, asyncio
+
+load_dotenv(override=True)
+
+async def main():
+    agent = Agent(
+        name="jessica",
+        prompt="you are a young woman named lily who says 'like' a lot",
+    )
+
+    scenario = Scenario(
+        name="order_donut",
+        prompt="order a dozen donuts with sprinkles and a coffee",
+        evaluations=[
+            Evaluation(name="order_success", prompt="the order was successful"),
+            Evaluation(name="price_confirmed", prompt="the agent confirmed the price of the order"),
+        ],
+    )
+
+    port = 8765
+    listener = await ngrok.forward(port, authtoken=os.getenv("NGROK_AUTH_TOKEN"), domain="api.jpixa.ngrok.dev") # type: ignore (needed or else python will complain)
+
+    test_runner = TestRunner(
+        port=port,
+        ngrok_url=listener.url(),
+        twilio_phone_number="+15554443333", # the twilio phone number to initiate calls from
+        evaluator=LocalEvaluator(),
+    )
+
+    test = Test(scenario=scenario, agent=agent)
+    test_runner.add_test(test)
+
+    result = await test_runner.run_tests(
+        type=TestRunner.OUTBOUND,
+        phone_number="+15554443333", # the phone number to call
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+```
+
+make sure to add the following api keys to your `.env` file:
+
+```bash
+OPENAI_API_KEY=
+DEEPGRAM_API_KEY=
+CARTESIA_API_KEY=
+TWILIO_ACCOUNT_SID=
+TWILIO_AUTH_TOKEN=
+NGROK_AUTH_TOKEN=
+```
+
+example output in the console:
+
+```bash
+✨ All tests completed!
+
+📊 Test Results:
+==================================================
+
+🎯 order_donut (jessica)
+🔊 Recording URL: https://api.twilio.com/XXX
+-- ✅ order_success: The order was successfully placed and confirmed by the user and the assistant.
+-- ❌ price_confirmed: The price of the order was not mentioned or confirmed during the conversation.
+
+==================================================
+```
+
+for more info, check out our docs
+
+for questions setting anything up, join our discord
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,73 +1,43 @@
 from fixa import Test, Agent, Scenario, Evaluation, TestRunner
-from fixa.evaluators import LocalEvaluator, CloudEvaluator
-import ngrok
-import os
+from fixa.evaluators import LocalEvaluator
 from dotenv import load_dotenv
-import asyncio
+import ngrok, os, asyncio
 
 load_dotenv(override=True)
 
-async def test_simple():
-    agents = [
-        Agent(
-            name="jessica",
-            prompt="you are a young woman named lily who says 'like' a lot",
-            voice_id="b7d50908-b17c-442d-ad8d-810c63997ed9"
-        ),
-        Agent(
-            name="steve",
-            prompt="you are a man named steve who had a bad day at work and is now angry",
-            voice_id="95856005-0332-41b0-935f-352e296aa0df"
-        ),
-        Agent(
-            name="marge",
-            prompt="you are an elderly woman named marge who sometimes gets carried away",
-        ),
-    ]
+async def main():
+    agent = Agent(
+        name="jessica",
+        prompt="you are a young woman named lily who says 'like' a lot",
+        voice_id="b7d50908-b17c-442d-ad8d-810c63997ed9"
+    )
 
-    scenarios = [
-        Scenario(
-            name="order_donut",
-            prompt="order a dozen donuts with sprinkles and a coffee",
-            evaluations=[
+    scenario = Scenario(
+        name="order_donut",
+        prompt="order a dozen donuts with sprinkles and a coffee",
+        evaluations=[
             Evaluation(name="order_success", prompt="the order was successful"),
-                Evaluation(name="price_confirmed", prompt="the agent confirmed the price of the order"),
-            ],
-        ),
-        Scenario(
-            name="ask_question",
-            prompt="ask a question about store hours",
-            evaluations=[
-                Evaluation(name="question_answered", prompt="the agent answered the question correctly and in a way that is helpful"),
-            ],
-        ),
-        Scenario(
-            name="ask_question_2",
-            prompt="ask a question about the store's address",
-            evaluations=[
-                Evaluation(name="question_answered", prompt="the agent answered the question correctly and in a way that is helpful"),
-            ],
-        ),
-    ]
+            Evaluation(name="price_confirmed", prompt="the agent confirmed the price of the order"),
+        ],
+    )
 
     port = 8765
-    listener = await ngrok.forward(port, authtoken=os.getenv("NGROK_AUTH_TOKEN"), domain="api.jpixa.ngrok.dev") # type: ignore
+    listener = await ngrok.forward(port, authtoken=os.getenv("NGROK_AUTH_TOKEN"), domain="api.jpixa.ngrok.dev") # type: ignore (needed or else python will complain)
 
     test_runner = TestRunner(
         port=port,
         ngrok_url=listener.url(),
-        twilio_phone_number=os.getenv("TWILIO_PHONE_NUMBER") or "",
-        # evaluator=CloudEvaluator(api_key=os.getenv("FIXA_API_KEY") or ""),
+        twilio_phone_number=os.getenv("TWILIO_PHONE_NUMBER") or "", # the twilio phone number to initiate calls from
         evaluator=LocalEvaluator(),
     )
 
-    for scenario in scenarios:
-        for agent in agents:
-            test = Test(scenario=scenario, agent=agent)
-            test_runner.add_test(test)
+    test = Test(scenario=scenario, agent=agent)
+    test_runner.add_test(test)
 
-    test_results = await test_runner.run_tests(type=TestRunner.OUTBOUND, phone_number=os.getenv("TEST_PHONE_NUMBER") or "")
-    # print(test_results)
+    result = await test_runner.run_tests(
+        type=TestRunner.OUTBOUND,
+        phone_number=os.getenv("TEST_PHONE_NUMBER") or "", # the phone number to call
+    )
 
 if __name__ == "__main__":
-    asyncio.run(test_simple())
+    asyncio.run(main())
